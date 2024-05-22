6aa0f86bf63eceddab8b60dbb819d2bae9f4ca65
Esoe B.Fildine <myebstudios@gmail.com>
2024-05-22 15:19:31 +0100

    Module 1 & 2

---
+++ Module 2: Displaying Text/Challenge1.py
@@ -0,0 +1,9 @@
+poem = """
+    There once was a movie star icon \n\twho preferred to sleep with the light on.
+    They learned how to code
+    a device that sure glowed
+    and lit up the night using Python!
+"""
+
+
+print(poem)
---
+++ Module 2: Displaying Text/DisplayingText.py
@@ -0,0 +1,14 @@
+print('Text 1')
+print("Text 2")
+print('Text "3"')
+print("Text '4'")
+print("""
+    This is a multiline
+    string with three \ndouble quotes
+""")
+print('''
+    This is a multiline
+    string with three single quotes
+''')
+print('Esoe ' + "Fildine")
+print('this is a \\ backslash character')
